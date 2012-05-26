9e49b1e577890e656c802d622b8d829c1494ea13
novito <roigarderiu@gmail.com>
2012-05-26 11:18:03 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mistutores::Application.config.secret_token = 'b6ad2a9d1a05436e4b451e2528d92c8ca473f3260c8a9266ca892c8780de359e70fd81bab209ebfefa8bdc3da77af0a43c17d286a0c11be91ce8f4683c333b13'
